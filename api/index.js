a432e9b594825ba8abc88a155729aa732a290514
Yosefgeda <100337086+Yosefgeda@users.noreply.github.com>
2023-12-12 13:12:25 +0300

    Created and connected to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express();
 
-
+const url = process.env.MONGO
+mongoose.connect(url).then(() => {
+    console.log('Connected To Mongodb')
+}).catch((err) => {
+    console.log(err)
+});
 
 app.listen(3000, () => console.log('Server started'))
