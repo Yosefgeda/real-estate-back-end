51d41f46dd833bf844aca73122aca8c746b711b7
Yosefgeda <100337086+Yosefgeda@users.noreply.github.com>
2023-12-12 18:50:37 +0300

    Create Route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 
+import userRoutes from './routes/user.routes.js';
+
 dotenv.config()
 
 const app = express();
@@ -12,5 +14,7 @@ mongoose.connect(url).then(() => {
 }).catch((err) => {
     console.log(err)
 });
+
+app.use('/api/users', userRoutes)
 
 app.listen(3000, () => console.log('Server started'))
