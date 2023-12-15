9bb7572c81d659702e3887a92855597b39c2027e
Yosefgeda <100337086+Yosefgeda@users.noreply.github.com>
2023-12-15 20:59:23 +0300

    Created route functionality for signIn

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/users.model.js";
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { errorHandlers } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
     const hashedPassword = bcryptjs.hashSync(req.body.password, 10)
@@ -19,4 +21,25 @@ export const signup = async (req, res, next) => {
         next(error)
         // res.status(500).json(error.message);
     } 
+}
+
+export const signin = async (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password
+    try {
+        const validUser = await User.findOne({ email }).exec();
+        console.log(validUser)
+        if(!validUser) return errorHandlers(401, 'Invalid Credentials')
+        const validPassword = bcryptjs.compareSync(password, validUser.password)
+        if(!validPassword) return errorHandlers(401, 'Invalid Credentials');
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+        const { password: hashedPassword, ...rest } = validUser._doc; // removing password from 
+        const expireDate = new Date(Date.now() + 3600000) // 1 hour
+        res
+            .cookie('access_token', token, { httpOnly: true, expires: expireDate } )
+            .status(200)
+            .json(rest)
+    } catch(error) {
+        next(error)
+    }
 }
