4f6db953fe77df2d40d1b7ac2342e5c08b463ca2
Yosefgeda <100337086+Yosefgeda@users.noreply.github.com>
2023-12-20 18:45:57 +0300

    Add Oauth functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import User from "../models/users.model.js";
 import bcryptjs from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import { errorHandlers } from "../utils/error.js";
+import hashSync from "bcryptjs";
 
 export const signup = async (req, res, next) => {
     const hashedPassword = bcryptjs.hashSync(req.body.password, 10)
@@ -27,8 +28,7 @@ export const signin = async (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password
     try {
-        const validUser = await User.findOne({ email }).exec();
-        console.log(validUser)
+        const validUser = await User.findOne({ email });
         if(!validUser) return errorHandlers(401, 'Invalid Credentials')
         const validPassword = bcryptjs.compareSync(password, validUser.password)
         if(!validPassword) return errorHandlers(401, 'Invalid Credentials');
@@ -40,6 +40,41 @@ export const signin = async (req, res, next) => {
             .status(200)
             .json(rest)
     } catch(error) {
+        // res.json(error)
         next(error)
+    }
+}
+
+export const google = async (req, res, next) => {
+    try {
+        const user = await User.findOne({email: req.body.email});
+        if(user) {
+            const token = jwt.sign({id: user._id}, process.env.JWT_SECRET);
+            const { password: hashedPassword, ...rest } = user._doc; // removing password from 
+            const expireDate = new Date(Date.now() + 3600000) // 1 hour
+            res
+            .cookie('access_token', token, { httpOnly: true, expires: expireDate })
+            .status(200)
+            .json(rest)
+        } else {
+            const generatedPassword = Math.random().toString(36).split(-8);
+            const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
+            const newUser = new User({
+                username: req.body.name.split(' ').join('').toLowerCase() + Math.floor(Math.random() * 10000).toString(),
+                email: req.body.email,
+                password: hashedPassword,
+                profilePicture: req.body.photo
+            });
+            await newUser.save();
+            const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+            const { password: hashedPassword2, ...rest } = newUser._doc; // removing password from 
+            const expireDate = new Date(Date.now() + 3600000) // 1 hour
+            res
+            .cookie('access_token', token, { httpOnly: true, expires: expireDate })
+            .status(200)
+            .json(rest);
+        }
+    } catch(error) {
+        next(error)            
     }
 }
--- api/models/users.model.js
+++ api/models/users.model.js
@@ -17,6 +17,10 @@ const userSchema = new Schema({
     password: {
         type: String,
         required: true,
+    },
+    profilePicture: {
+        type: String,
+        default: '',
     }
 
 }, { timestamps: true });
